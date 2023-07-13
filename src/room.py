3de7cac4d47e2e2a4f93c29d6bbdcd1af5021c3c
James Dickson <jamesstuartdickson@gmail.com>
2023-07-13 15:27:07 +0100

    added fee and money_spent tracker in rooms, added charge_fee method to room and updated it to be incldued in chek_in_guest

--- src/room.py
+++ src/room.py
@@ -1,12 +1,15 @@
 class Room:
-    def __init__(self, name, room_limit):
+    def __init__(self, name, room_limit, fee):
         self.name = name
         self.list_of_songs = []
         self.guests_in_room = []
         self.room_limit = room_limit
+        self.fee = fee
+        self.money_spent_in_room = 0
     
     def check_in_guest(self, guest):
         if len(self.guests_in_room) < 4:
+            self.charge_fee(guest, self.fee)
             self.guests_in_room.append(guest)
 
     def check_out_guest(self, guest):
@@ -14,3 +17,7 @@ class Room:
 
     def add_song(self,song):
         self.list_of_songs.append(song)
+    
+    def charge_fee(self, guest, amount):
+        guest.pay_fee(amount)
+        self.money_spent_in_room += amount
--- tests/room_test.py
+++ tests/room_test.py
@@ -6,7 +6,7 @@ from src.song import Song
 class TestRoom(unittest.TestCase):
     
     def setUp(self):
-        self.room1 = Room("Room 1", 4)
+        self.room1 = Room("Room 1", 4, 10)
         self.guest1 = Guest("Mike", 25)
         self.song1 = Song("Rock and Roll McDonalds")
 
@@ -30,3 +30,8 @@ class TestRoom(unittest.TestCase):
         self.room1.check_in_guest(self.guest1) # adding in the same guest because it was faster than making multiple seperate guests
         self.room1.check_in_guest(self.guest1) # extra guest
         self.assertEqual([self.guest1, self.guest1, self.guest1, self.guest1], self.room1.guests_in_room)
+    
+    def test_charge_fee(self):
+        self.room1.check_in_guest(self.guest1)
+        self.assertEqual(15, self.guest1.wallet)
+        self.assertEqual(10, self.room1.money_spent_in_room)
